09de8255c8e78e0d8baa83dfa861e963abc5ac74
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-14 22:23:47 +0700

    fix image

--- src/component/ButtonNav.jsx
+++ src/component/ButtonNav.jsx
@@ -6,7 +6,7 @@ const ButtonNav = () => {
     return (
         <>
             <div className="button-Nav d-flex justify-content-center gap-3 p-5">
-                <Link to='' className="card text-decoration-none px-5 py-1">About Me</Link>
+                <Link to='' className="card text-decoration-none px-5 py-2">About Me</Link>
                 <Link to='my-skill' className="card text-decoration-none px-5 py-2">My Skill</Link>
                 <Link to='my-certif' className="card text-decoration-none px-5 py-2">Certificate</Link>
             </div>
