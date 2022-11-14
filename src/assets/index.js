595e16094abfc5380dcc3aa651cdaa43c393d088
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-14 22:02:15 +0700

    fix bug image

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,10 +8,10 @@ import sunrisePay from './sunrisePay.png'
 import bmiKalku from './bmiKalku.png'
 import vector from './vector1.png'
 
-import sertfikatHtml from './Skilvul-HTML-Dasar.png'
-import sertifikatCSS from './Skivul-css-dasar.png'
-import sertifikatJs from './Skilvul-Javasript-Dasar.png'
-import sertifikatWeb from './Skilvul-development-pemula.png'
+import sertfikatHtml from './SkilvulHtmlDasar.png'
+import sertifikatCSS from './SkilvulCssDasar.png'
+import sertifikatJs from './SkilvulJavascriptDasar.png'
+import sertifikatWeb from './SkilvulDevelopmentPemula.png'
 
 
 export {tafsirQ, bandaiNamco, cocaCola, personalWeb, airBnb, belajarAbc, sunrisePay, bmiKalku, vector, sertfikatHtml, sertifikatCSS, sertifikatJs, sertifikatWeb}
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ import './../CSS/Footer.css'
 const Footer = () => {
     return (
         <>
-            <div className="footer container-fluid">
+            <div className="footer container-fluid d-flex align-items-center ">
                 <div className="container d-flex justify-content-between align-items-center">
                     <div>Copyright@2022</div>
                     <MediaSocial />
