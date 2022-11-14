919190eb4d675ad3fee355fa8b9ce58ee7278a5b
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-14 21:28:43 +0700

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,12 @@
 import tafsirQ from './tafsirQ.png'
 import bandaiNamco from './bandai.png'
 import cocaCola from './cocaCola.png'
-import personalWeb from '/personal-web.png'
+import personalWeb from '/personalWeb.png'
 import airBnb from './airBnb.png'
 import belajarAbc from './berlajarAbc.png'
 import sunrisePay from './sunrisePay.png'
 import bmiKalku from './bmiKalku.png'
-import vector from './vector-1.png'
+import vector from './vector1.png'
 
 
 export {tafsirQ, bandaiNamco, cocaCola, personalWeb, airBnb, belajarAbc, sunrisePay, bmiKalku, vector}
